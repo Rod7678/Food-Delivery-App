4bbf8cd1bec09999d020ec164a32042442820f5f
Rohit <rohitghadge348@gmail.com>
2025-11-11 00:08:53 +0530

    added cart modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
+import { useRef } from "react";
+import CartModal from "./CartModal.jsx";
+
 export default function Header(){
+    const modal = useRef();
+    function handleOpenCartClick(){
+        modal.current.open();
+    }
+
+
     return (
+        <>
+        <CartModal 
+        ref={modal}
+        title="your cart"
+        />
         <section id="main-header">
-            <h3 id="title">menu</h3>
-            <button className="button">Add to cart</button>
+            <div id="title">
+                <img src={"/logo.jpg"} alt="" />
+                <h1>Rodfood</h1>
+            </div>
+            <button className="button" onClick={handleOpenCartClick}>Cart</button>
         </section>
+        </>
     )
 }
